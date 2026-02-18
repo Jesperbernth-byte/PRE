7c17182c2827de5f1c272dc06138296dcb96accc
Jesper Bernth <jesperbernth@icloud.com>
2026-02-18 15:38:14 +0100

    Tilføj Vercel Analytics

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import LeadChat from './components/LeadChat';
@@ -85,6 +86,7 @@ const App: React.FC = () => {
         </main>
         <Footer />
         <LeadChat />
+        <Analytics />
       </div>
     </Router>
   );
